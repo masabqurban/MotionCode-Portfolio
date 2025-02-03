4e31515c5f80b474195586c5d09e56080c864972
Masab Qurban <masabqurban@gmail.com>
2025-02-04 00:16:46 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -162,22 +162,23 @@ function App() {
           <h2 className='text-3xl sm:text-[40px] bg-[#ececec] dark:bg-[#111] relative z-10 font-bold font-lexend px-4 py-2 w-max mx-auto text-center text-[#1f2667e6] dark:text-[#1595b6] sm:border-b-2 border-[#1f2667e6] dark:border-[#1595b6]'> Latest Projects </h2>
 
           <div className='flex flex-col sm:flex-row gap-4 mt-8 sm:gap-[80px] items-center sm:mt-20 relative'>
-            <div className='h-[1px] left-1/4 right-1/2 bg-[#d9784e] absolute top-1/2 hidden sm:block'>
+            <div className='h-[1px] left-1/4 right-1/2 bg-[#9c3507] dark:bg-[#d9784e] absolute top-1/2 hidden sm:block'>
             </div>
-            <div className='w-4 h-4 rounded-full border-[3px] border-[#d9784e] absolute left-1/2 -translate-x-1/2 bg-[#ececec] dark:bg-[#111] z-10 hover:scale-110 ease-in-out duration-100 hidden sm:block'>
+            <div className='w-4 h-4 rounded-full border-[3px] border-[#9c3507] dark:border-[#d9784e] absolute left-1/2 -translate-x-1/2 bg-[#ececec] dark:bg-[#111] z-10 hover:scale-110 ease-in-out duration-100 hidden sm:block'>
             </div>
             <a href="https://tool-matrix.vercel.app/" className='flex w-full relative justify-center sm:justify-start'>
               <div className='flex flex-col items-center relative group sm:hover:scale-105 ease-in-out duration-200'>
                 <img src={laptop1dark} alt="" className='max-w-7xl w-[400px] mr-auto relative z-10 drop-shadow-[0_0px_60px_rgba(59,130,246,0.6)] hidden dark:block' />
                 <img src={laptop1light} alt="" className='max-w-7xl w-[400px] mr-auto relative z-10 drop-shadow-[0_0px_60px_rgba(59,130,246,0.6)] dark:hidden' />
-                <span className='font-lexend flex group-hover:-top-14 ease-jump duration-200 bg-[#d9784e] text-black sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded-md w-max items-center gap-1 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2 transition-all'> Tool Matrix
-                  <img src={blacklink} alt="" className='feather feather-external-link' />
+                <span className='font-lexend flex group-hover:-top-14 ease-jump duration-200 bg-[#9c3507] dark:bg-[#d9784e] text-white dark:text-black sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded-md w-max items-center gap-1 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2 transition-all'> Tool Matrix
+                  <img src={blacklink} alt="" className='feather feather-external-link hidden dark:block' />
+                  <img src={link} alt="" className='feather feather-external-link dark:hidden' />
                 </span>
               </div>
             </a>
             <div className='w-full'>
-              <h3 className='text-[#d9784e] font-bold text-2xl md:text-4xl font-lexend'> Tool Matrix </h3>
-              <span className='text-[#d9784e] text-base md:text-lg font-HubotSans font-medium' > (Final Year Project)</span>
+              <h3 className='text-[#9c3507] dark:text-[#d9784e] font-bold text-2xl md:text-4xl font-lexend'> Tool Matrix </h3>
+              <span className='text-[#9c3507] dark:text-[#d9784e] text-base md:text-lg font-HubotSans font-medium' > (Final Year Project)</span>
               <p className='text-black dark:text-white text-justify text-sm md:text-base mt-2 font-HubotSans '> A web-based platform developed in React JS, Node JS and Tailwind CSS featuring calculators for various domains. Integrated a Python-powered AI model utilizing Gemini API to solve handwritten math problems. Achieved 95% accuracy in solving math queries through the AI-powered handwriting recognition feature. </p>
               <ul className='flex flex-wrap gap-2 mt-2 text-black dark:text-white'>
                 <li className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base"> Vite </li>
@@ -194,7 +195,7 @@ function App() {
 
           <div className="flex flex-col sm:flex-row gap-4 mt-12 sm:gap-[80px] items-center sm:mt-20 relative" >
             <div className="order-2 sm:order-1 w-full">
-              <h3 className="text-[#905bf5] font-bold text-2xl md:text-4xl font-lexend"> Portfolio </h3>
+              <h3 className="text-[#905bf5] font-bold text-2xl md:text-4xl font-lexend">Portfolio </h3>
               <span className="text-[#905bf5] text-base md:text-lg font-HubotSans font-medium" >(A Modern Showcase of My Work)</span >
               <p className="text-black dark:text-white text-justify text-sm md:text-base mt-2 font-HubotSans">A sleek and interactive portfolio showcasing my skills with smooth animations, engaging UI, and modern design elements. Built with React.js and Tailwind CSS, enhanced by AOS, Framer Motion, and Material UI for a dynamic experience.</p>
               <ul className="flex flex-wrap gap-2 mt-2 text-black dark:text-white">
